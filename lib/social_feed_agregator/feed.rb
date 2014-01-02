90e0e87f904d544b1a5d6ae2af962050e2f7e1a3
Filipe Dias <f.dias@karma-consulting.pt>
2014-01-02 19:18:11 +0000

    Removes unattended changes.

--- lib/social_feed_agregator/feed.rb
+++ lib/social_feed_agregator/feed.rb
@@ -1,21 +1,22 @@
 module SocialFeedAgregator
   class Feed
 
-    attr_accessor :feed_type,
-      :feed_id,
-      :user_id,
-      :user_name,
-      :permalink,
-      :description,
-      :picture_url,
-      :name,
-      :link,
-      :message,
-      :caption,
-      :created_at,
-      :type
-
-    def initialize(options={})
+    attr_accessor :feed_type,      # :facebook, :twitter or :linkedin
+                  :feed_id,        # origin feed id
+                  :user_id,        # user id
+                  :user_name,      # user name
+                  :permalink,      
+                  :description,    
+                  :picture_url,    
+                  :name,
+                  :link, 
+                  # :story, 
+                  :message,          # status message
+                  :caption, 
+                  :created_at, 
+                  :type              # link or status
+    
+    def initialize(options={}) 
       @feed_type = options[:feed_type]
       @feed_id =   options[:feed_id]
 
@@ -28,14 +29,11 @@ module SocialFeedAgregator
 
       @name  = options[:name]
       @link  = options[:link]
+      # @story = options[:story]
       @message = options[:message]
       @caption = options[:caption]
       @created_at = options[:created_at]
-      @type = options[:type]
-    end
-
-    def attributes
-      instance_variables.inject({}) { |r, a| r[a.to_s.gsub("@","")] = instance_variable_get(a).to_s; r }
+      @type = options[:type]              
     end
 
   end
